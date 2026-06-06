5884ea9d3ae89cdf17b610bf9c823fd37055987d
VIKAS_PATEL <166948664+VikasPatel2004@users.noreply.github.com>
2026-06-06 17:43:59 +0530

    debugging github api error came on webservice

--- src/services/githubService.js
+++ src/services/githubService.js
@@ -2,8 +2,13 @@ const axios = require("axios");
 
 const getGithubProfile = async (username) => {
   const response = await axios.get(
-    `https://api.github.com/users/${username}`
-  );
+  `https://api.github.com/users/${username}`,
+  {
+    headers: {
+      "User-Agent": "Github-Profile-Analyzer"
+    }
+  }
+);
 
   return response.data;
 };
